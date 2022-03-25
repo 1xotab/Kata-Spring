c4bcb341fdf3d7d8ec9b1a2718dafc3223d753f2
1xotab <ninetonine319@gmail.com>
2022-03-25 20:00:17 +0300

    Initial commit

--- src/main/java/AppConfig.java
+++ src/main/java/AppConfig.java
@@ -4,7 +4,15 @@ import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class AppConfig {
- 
+
+    @Bean(name = "cat")
+    @Scope(scopeName = "prototype")
+    public Cat setName() {
+        Cat cat = new Cat();
+        cat.setName("Ivan");
+        return cat;
+    }
+
     @Bean(name="helloworld")
     public HelloWorld getHelloWorld() {
         HelloWorld helloWorld = new HelloWorld();
